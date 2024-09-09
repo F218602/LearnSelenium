e0cb46e146dd0d4946ac4bf5211ac3fa1aaf72f9
Anisha Jeni Ravi Sam <jenisam45454@gmail.com>
2024-09-09 22:00:54 +0100

    First Commit

--- src/test/java/seleniumScripts/FirstProgram.java
+++ src/test/java/seleniumScripts/FirstProgram.java
@@ -4,7 +4,7 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Irashaimase!");
+		System.out.println("Irashaimase Nihon!!!!!!");
 	}
 
 }
